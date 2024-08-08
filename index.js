bc6eb275fb48535ecc66e33741a46e36f849fc3c
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 18:55:24 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Declare and assign a string variable storing your favorite food with the let keyword (pick any appropriate name!)
+let deniFavFood = "Sesame chicken"
+
+// Console log the variable
+ console.log(deniFavFood)
+// Declare and assign a string variable storing your birth year with the const keyword (pick any appropriate name!)
+
+const birthYear = 2007
+
+// Cast the birth year variable to be a number (int or float)
+
+parseInt=(birthYear)
+
+// Console log the variable
+
+console.log(birthYear)
+// Declare but do NOT assign a variable called examRetakeGrade
+let examRetakeGrade;
+
+// Console log the type of examRetakeGrade
+ console.log(typeof(examRetakeGrade))
+// Declare and assign a variable storing bad questions and set it to null (pick any appropriate name!)
+
+let badQuestions= null
+
+// Declare and assign a boolean variable storing whether you feel ready for the exam retake (pick any appropriate name!)
+
+let prepared= true
+
+// Console log the variable
+console.log(prepared)
+
